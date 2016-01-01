23a1fe9b8f9ce0a58419b45e6ef123141470eee4
blakeparkinson <wizardplow@gmail.com>
2016-01-01 00:51:13 -0700

    MP

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -33,6 +33,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/mixpanel/mixpanel-jslib-snippet.js',
+      'bower_components/angular-mixpanel/src/angular-mixpanel.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
